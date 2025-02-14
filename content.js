89910c2c764f3c44500457897f133b2dac17131a
Brian Kresge <brian.kresge@gmail.com>
2025-02-13 21:16:48 -0500

    Extension breaks Facebook, so adding hostname exclusion

--- content.js
+++ content.js
@@ -2,6 +2,10 @@
 
 // Function to replace all occurrences of the phrase "Gulf of America" with "Gulf of Mexico"
 function replaceText() {
+    if (window.location.hostname.includes("facebook.com")) {
+        console.log("Skipping Facebook page.");
+        return; // Skip replacing text if the page is from Facebook
+    }
     // Get the HTML content of the entire body of the page
     const bodyText = document.body.innerHTML;
   
